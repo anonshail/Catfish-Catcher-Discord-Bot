932a5cc2c4d858a38ae1c90af17c13380b613760
Shail <anonshail@gmail.com>
2020-02-05 22:54:20 +0530

    WRIP

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+//reverse image search API
+const reverseImageSearch = require('reverse-image-search-google')
+
+//function that will obtain results
+const reverseImageSearchResults = (results) => {
+    console.log(results)
+}
+
+//obtaining token and prefix from the config file
+const { prefix, token } = require('./config.json');
+
+//displaying status once bot has logged in
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login('your-token-goes-here');
+//logging in 
+client.login(token);
