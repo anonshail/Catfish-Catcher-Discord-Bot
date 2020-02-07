0e4138c2aa2f76163a02e11a83defba91b0d4786
Shail <anonshail@gmail.com>
2020-02-07 18:50:49 +0530

    working f*cking bot

--- index.js
+++ index.js
@@ -4,26 +4,107 @@ const client = new Discord.Client();
 //reverse image search API
 const reverseImageSearch = require('reverse-image-search-google')
 
-//function that will obtain results
-const reverseImageSearchResults = (results) => {
-    console.log(results.url)
-}
-
 //obtaining token and prefix from the config file
 const { prefix, token } = require('./config.json');
 
 //displaying status once bot has logged in
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
 });
+
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+}
 
 //logging in 
 client.login(token);
 
-//on message
+//on receiving a message, check if it is in bot-testing, and chech syntax
 client.on('message', function(message){
-    let atch = (message.attachments).array();
-    atch.forEach(function(attachment) {
-        reverseImageSearch(attachment.url, reverseImageSearchResults);
-    })
+
+    
+
+    //if channel is sent on relevant command channel?
+    if(message.channel.name === 'detectivebot'){
+        let newMsg = message.content
+
+        //if newMsg begins with ${prefix}reverse
+        if(newMsg.startsWith(`${prefix}reverse`)){
+            //reset reportString and numOfHits
+
+            //check validity of command, syntax, and then execute
+            
+            command = newMsg.split(" ");
+            numOfMessages = command[1]
+            channelID = command[2]
+            channelID = channelID.slice(2,-1)
+
+            //error checking, check the number, and the channel
+            if(isNaN(numOfMessages)){
+                message.reply("Please enter a valid number!\nFormat: !reverse <numberOfMsgs> <channel>")
+            }
+
+            //checking if channel is valid
+            if(message.guild.channels.find(val => val.id === channelID) == null){
+                message.reply("Please enter a valid channel!\nFormat: !reverse <numberOfMsgs> <channel>")
+            }
+
+            //now, look up the numOfMessages from the channel and apply reverse search
+            let targetChannel = message.mentions.channels.first() //got target channel - assume first
+            targetChannel.fetchMessages({ limit: Number(numOfMessages) })
+            .then(messages => {
+                //lookup all messages and check for image attachments and do reverse image search
+
+                let listOfMsgs = messages.array()
+                listOfMsgs.forEach( (msg, index) => {
+                    let attaches = (msg.attachments).array();
+                    attaches.forEach(async function(attachment) {
+                        let url = attachment.url
+                        if(url.endsWith(".jpg")
+                            ||url.endsWith(".png")
+                            ||url.endsWith(".jpeg")
+                            ||url.endsWith(".gif")
+                            ||url.endsWith(".bmp")
+                        ){
+                            //if the url is an image, look it up!
+                            
+                            //function that will obtain results
+                            const reverseImageSearchResults1 = (results) => {
+                                message.channel.send(`Results for the user **${msg.author.tag}**:`)
+                                message.channel.send(results.url)
+                            }
+                            reverseImageSearch(attachment.url, reverseImageSearchResults1)
+                        }
+                    })
+                })
+            }).catch(console.error);
+            
+        }
+
+        if(newMsg.startsWith(`${prefix}lookup`)){
+            command = newMsg.split(" ");
+            
+            //argument must be a url
+            let url = command[1]
+
+
+            //function that will obtain results
+            const reverseImageSearchResults2 = (results) => {
+                message.channel.send(`Results for the url:`)
+                message.channel.send(results.url)
+            }
+
+            if(validURL(url)){
+                reverseImageSearch(url, reverseImageSearchResults2)
+            }
+
+        }
+
+    }
 })
